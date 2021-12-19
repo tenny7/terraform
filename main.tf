c4846d5ba2f8540dca4192b0ae816a4a9c3413dc
Ovie Tennyson <atomictenny@gmail.com>
2021-12-19 04:03:27 +0000

    Restructured Configuration

--- main.tf
+++ main.tf
@@ -1,163 +1,37 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.0.0"
-    }
-  }
+resource "azurerm_resource_group" "resourcegroup" {
+  name     = var.azure_rg_name
+  location = var.azure_rg_location
 }
 
-provider "azurerm" {
-  features { }
-}
-
-# reource group for function App and storage accounts
-resource "azurerm_resource_group" "tfresourcegroup" {
-  name     = "terraform_rg"
-  location = "West Europe"
-}
-
-# app service plan for function App
-resource "azurerm_app_service_plan" "myappserviceplan" {
-  name                = "azure-functions-repro-service-plan"
-  location            = azurerm_resource_group.tfresourcegroup.location
-  resource_group_name = azurerm_resource_group.tfresourcegroup.name
+resource "azurerm_app_service_plan" "asp" {
+  name                = var.azure_asp_name
+  location            = var.azure_rg_location
+  resource_group_name = var.azure_rg_name
 
   sku {
-    tier = "Premium"
-    size = "P1"
+    tier = var.azure_asp_sku_tier
+    size = var.azure_asp_sku_size
   }
 }
 
-# function app
-resource "azurerm_function_app" "terrafuncrepro" {
-  name                       = "repro-azure-functions"
-  location                   = azurerm_resource_group.tfresourcegroup.location
-  resource_group_name        = azurerm_resource_group.tfresourcegroup.name
-  app_service_plan_id        = azurerm_app_service_plan.myappserviceplan.id
-  storage_account_name       = azurerm_storage_account.tfchisomstorage.name
-  storage_account_access_key = azurerm_storage_account.tfchisomstorage.primary_access_key
+resource "azurerm_app_service" "appservice" {
+  name                = var.azure_app_name
+  location            = var.azure_rg_location
+  resource_group_name = var.azure_rg_name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
 
-   # dependency on function App storage being created for function App to work
-  depends_on = [
-    azurerm_storage_account.tfchisomstorage
-  ] 
-
-  # app_settings = {
-  #   "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING" = azurerm_storage_account.tfchisomstorage.primary_connection_string
-  #   "AzureWebJobsStorage"                      = azurerm_storage_account.tfchisomstorage.primary_connection_string
-  #   "WEBSITE_VNET_ROUTE_ALL"                   = "1"
-  #   "WEBSITE_CONTENTSHARE"                     = azurerm_storage_share.azfile.name
-  #   "WEBSITE_CONTENTOVERVNET"                  = "1"
-  #   "WEBSITE_DNS_SERVER"                       = "168.63.129.16"
-  # }
-
-}
-
-# vnet for resources
-resource "azurerm_virtual_network" "tfvnet" {
-  name                = "terraformvirtualnetwork"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.tfresourcegroup.location
-  resource_group_name = azurerm_resource_group.tfresourcegroup.name
-}
-
-# subnet for the virtual network
-resource "azurerm_subnet" "tfsubnet" {
-  name                 = "terraform_subnetname"
-  resource_group_name  = azurerm_resource_group.tfresourcegroup.name
-  virtual_network_name = azurerm_virtual_network.tfvnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
-
-  delegation {
-    name = "my-subnet-delegation"
-
-    service_delegation {
-      name    = "Microsoft.Web/serverFarms"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
-  }
-}
-
-# creating the storage account for the Vnet
-resource "azurerm_storage_account" "tfchisomstorage" {
-  name                = "tfchisomstorage"
-  resource_group_name = azurerm_resource_group.tfresourcegroup.name
-  location                 = azurerm_resource_group.tfresourcegroup.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  network_rules {
-    default_action             = "Allow"
-    ip_rules = ["20.67.29.115"]
-    virtual_network_subnet_ids = [azurerm_subnet.tfsubnet.id]
-    # bypass = [
-    #   "Metrics",
-    #   "Logging",
-    #   "AzureServices"
-    # ]
+  site_config {
+    dotnet_framework_version = var.site_config_dotnet_ver
+    scm_type                 = var.site_config_scm_type
   }
 
-  tags = {
-    environment = "staging"
+  app_settings = {
+    "SOME_KEY" = var.app_settings_value
   }
 
-  # provisioner "local-exec" {
-  #   command =<<EOT
-  #   az storage share create `
-  #   --account-name ${azurerm_storage_account.tfchisomstorage.name} `
-  #   --account-key ${azurerm_storage_account.tfchisomstorage.primary_access_key} `
-  #   --name ${var.myshare} `
-  #   --quota 100   
-  #   EOT
-
-  #   interpreter = [ "Powershell", "-c"]
-  # }
-
-}
-
-# resource "azurerm_storage_share" "azfile" {
-#   name                 = "testsharename"
-#   storage_account_name = azurerm_storage_account.tfchisomstorage.name
-#   quota                = 50
-
-#   depends_on = [
-#     azurerm_storage_account.tfchisomstorage
-#   ]
-# }
-
-
-
-resource "azurerm_function_app_slot" "slt" {
-  name                       = "deslot"
-  location                   = azurerm_resource_group.tfresourcegroup.location
-  resource_group_name        = azurerm_resource_group.tfresourcegroup.name
-  app_service_plan_id        = azurerm_app_service_plan.myappserviceplan.id
-  function_app_name          = azurerm_function_app.terrafuncrepro.name
-  storage_account_name       = azurerm_storage_account.tfchisomstorage.name
-  storage_account_access_key = azurerm_storage_account.tfchisomstorage.primary_access_key
-
-  # app_settings = {
-  #   "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING" = azurerm_storage_account.tfchisomstorage.primary_connection_string
-  #   "AzureWebJobsStorage"                      = azurerm_storage_account.tfchisomstorage.primary_connection_string
-  #   "WEBSITE_VNET_ROUTE_ALL"                   = "1"
-  #   "WEBSITE_CONTENTSHARE"                     = azurerm_storage_share.azfile.name
-  #   "WEBSITE_CONTENTOVERVNET"                  = "1"
-  #   "WEBSITE_DNS_SERVER"                       = "168.63.129.16"
-  # }
-  depends_on = [
-    azurerm_storage_account.tfchisomstorage
-  ]
-}
-
-resource "azurerm_app_service_virtual_network_swift_connection" "swift_connection" {
-  app_service_id = azurerm_function_app.terrafuncrepro.id
-  subnet_id      = azurerm_subnet.tfsubnet.id
-}
-
-resource "azurerm_app_service_slot_virtual_network_swift_connection" "myslot" {
-  slot_name      = azurerm_function_app_slot.slt.name
-  app_service_id = azurerm_function_app.terrafuncrepro.id
-  subnet_id      = azurerm_subnet.tfsubnet.id
+  connection_string {
+    name  = var.app_conn_string_name
+    type  = var.app_conn_string_type
+    value = var.app_conn_string_value
+  }
 }
