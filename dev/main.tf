741d49645331efa5f956e1ce3a206c32dacef171
Ovie Tennyson <atomictenny@gmail.com>
2021-12-19 05:24:31 +0000

    Bug fix

--- dev/main.tf
+++ dev/main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "resourcegroup" {
+resource "azurerm_resource_group" "terraresourcegroup" {
   name     = var.azure_rg_name
   location = var.azure_rg_location
 }
