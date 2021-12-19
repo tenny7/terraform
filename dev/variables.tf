5b2c254e2de90f37e9d3c03de2e8bed50575db8d
Ovie Tennyson <atomictenny@gmail.com>
2021-12-19 02:40:49 +0000

    Modified variabkes.tf

--- dev/variables.tf
+++ dev/variables.tf
@@ -1,53 +1,53 @@
 # resource group variables
 variable "azure_rg_name" {
-    type = "string"
+    type = string
 }
 
 variable "azure_rg_location" {
-    type = "string"
+    type = string
 }
 
 
 # app service plan variables
 variable "azure_asp_name" {
-    type = "string"
+    type = string
 }
 
 variable "azure_asp_sku_tier" {
-    type = "string"
+    type = string
 }
 
 variable "azure_asp_sku_size" {
-    type = "string"
+    type = string
 }
 
 
 # app service variables
 variable "azure_app_name" {
-    type = "string"
+    type = string
 }
 
 variable "site_config_dotnet_ver" {
-    type = "string"
+    type = string
 }
 
 variable "site_config_scm_type" {
-    type = "string"
+    type = string
 }
 
 variable "app_settings_value" {
-    type = "string"
+    type = string
 }
 
 variable "app_conn_string_name" {
-    type = "string"
+    type = string
 }
 
 variable "app_conn_string_type" {
-    type = "string"
+    type = string
 }
 
 variable "app_conn_string_value" {
-    type = "string"
+    type = string
 }
 
