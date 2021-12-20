e96ec5649a697aabb1770cb79ca68bbdc1b3863b
Ovie Tennyson <atomictenny@gmail.com>
2021-12-20 02:16:29 +0000

    Initial Push

---
+++ main.tf
@@ -0,0 +1,47 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "terra-state-stor-rg"
+    storage_account_name  = "terrastorr"
+    container_name        = "terracontainer"
+    key                   = "terraform.tfstate"
+  }
+}
+
+
+resource "azurerm_resource_group" "resourcegroup" {
+  name     = var.azure_rg_name
+  location = var.azure_rg_location
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = var.azure_asp_name
+  location            = var.azure_rg_location
+  resource_group_name = var.azure_rg_name
+
+  sku {
+    tier = var.azure_asp_sku_tier
+    size = var.azure_asp_sku_size
+  }
+
+  depends_on = [
+    azurerm_resource_group.resourcegroup
+  ]
+
+}
+
+resource "azurerm_app_service" "appservice" {
+  name                = var.azure_app_name
+  location            = var.azure_rg_location
+  resource_group_name = var.azure_rg_name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+
+  depends_on = [
+    azurerm_app_service_plan.asp
+  ]
+
+
+}
---
+++ variables.tf
@@ -0,0 +1,65 @@
+# resource group variables
+variable "azure_rg_name" {
+    type = string
+    default = "dev_weu_rg"
+}
+
+variable "azure_rg_location" {
+    type = string
+    default = "West Europe"
+}
+
+
+# app service plan variables
+variable "azure_asp_name" {
+    type = string
+    default = "terrates-asp"
+}
+
+variable "azure_asp_sku_tier" {
+    type = string
+    default = "Standard"
+}
+
+variable "azure_asp_sku_size" {
+    type = string
+    default = "S1"
+}
+
+
+# app service variables
+variable "azure_app_name" {
+    type = string
+    default = "terra-dev-app-01"
+}
+
+variable "site_config_dotnet_ver" {
+    type = string
+    default = "v4.0"
+}
+
+variable "site_config_scm_type" {
+    type = string
+    default = "LocalGit"
+}
+
+variable "app_settings_value" {
+    type = string
+    default = "some-value"
+}
+
+variable "app_conn_string_name" {
+    type = string
+    default = "Database"
+}
+
+variable "app_conn_string_type" {
+    type = string
+    default = "SQLServer"
+}
+
+variable "app_conn_string_value" {
+    type = string
+    default = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+}
+
